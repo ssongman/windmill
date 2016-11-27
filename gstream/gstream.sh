eab7b95648b264bfcec67d18c168eea0caf0e3fd
ssongman <ssongmantop@gmail.com>
2016-11-27 14:48:44 +0900

    robot5.py - webcam gstream

--- gstream/gstream.sh
+++
@@ -1,2 +0,0 @@
-raspivid -fps 25 -h 720 -w 1080  -n -t 0 -b 200000 -o - | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host=192.168.43.229 port=5000
-#raspivid -fps 25 -h 720 -w 1080  -n -t 0 -b 200000 -o - | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host=192.168.0.48 port=5000
